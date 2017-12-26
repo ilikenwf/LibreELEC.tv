1f002e1144bda97281107eb72be66267a8a86379
Matt Parnell <parwok@gmail.com>
2017-12-26 03:48:09 +0000

    update

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="a9a7a20"
+PKG_VERSION="d4734f"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
